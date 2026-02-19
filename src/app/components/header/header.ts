1f171776d9555580e2201265d912d0cbc16d3075
XuanNam58 <104215250+XuanNam58@users.noreply.github.com>
2026-02-19 16:51:17 +0700

    Create home component

---
+++ src/app/components/header/header.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header implements OnInit{
+  constructor() {}
+  ngOnInit(): void {
+    
+  }
+}
